71c1b0faa3d005a0a4c45665e4119598a814b3a9
Xcel-Host <shyam.mishra0010@gmail.com>
2026-05-23 11:32:01 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Explore from './pages/Explore'
+import SectorDetail from './pages/SectorDetail'
+import Alternatives from './pages/Alternatives'
+import Participate from './pages/Participate'
+import Philosophy from './pages/Philosophy'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore/:group" element={<Explore />} />
+        <Route path="/sector/:group/:sectorId" element={<SectorDetail />} />
+        <Route path="/alternatives" element={<Alternatives />} />
+        <Route path="/participate" element={<Participate />} />
+        <Route path="/philosophy" element={<Philosophy />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
